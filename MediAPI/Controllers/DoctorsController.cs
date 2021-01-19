7a53a82ed4027e0af926ad7f54aa226c44feaed8
binas <matib95@gmail.com>
2021-01-19 20:22:04 +0100

    feature: Add basic data and method to return all doctors

--- MediAPI/Controllers/DoctorsController.cs
+++ MediAPI/Controllers/DoctorsController.cs
@@ -1,4 +1,6 @@
-﻿using MediAPI.Helpers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MediAPI.Helpers;
 using MediAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,6 +13,12 @@ namespace MediAPI.Controllers
     {
         private readonly ILogger<DoctorsController> _logger;
 
+        private readonly Doctor[] _doctors = {
+            new Doctor {Id = 1, Name = "Martha", Surname = "Pro", Specialization = Specialization.Trainee},
+            new Doctor {Id = 2, Name = "Tom", Surname = "Doc", Specialization = Specialization.FamilyDoctor},
+            new Doctor {Id = 3, Name = "James", Surname = "Sub", Specialization = Specialization.Gynecology}
+        };
+
         public DoctorsController(ILogger<DoctorsController> logger)
         {
             _logger = logger;
@@ -19,13 +27,13 @@ namespace MediAPI.Controllers
         [HttpGet("{id}")]
         public Doctor Get(int id)
         {
-            return new Doctor
-            {
-                Id = id,
-                Name = "Martha",
-                Surname = "Pro",
-                Specialization = Specialization.Trainee
-            };
+            return _doctors.FirstOrDefault((d) => d.Id == id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Doctor> Get()
+        {
+            return _doctors;
         }
     }
 }
--- MediAPITests/DoctorsControllerTests.cs
+++ MediAPITests/DoctorsControllerTests.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
+using System.Linq;
 using FakeItEasy;
 using MediAPI.Controllers;
-using MediAPI.Helpers;
-using MediAPI.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
@@ -15,34 +13,29 @@ namespace MediAPITests
     {
         private ILogger<DoctorsController> _logger;
         private DoctorsController _doctorsController;
-        private List<Doctor> _doctors;
         
         [SetUp]
         public void Setup()
         {
             _logger = A.Fake<ILogger<DoctorsController>>();
-            _doctors = GetTestDoctors();
             _doctorsController = new DoctorsController(_logger);
         }
         
         [Test]
-        public void Should_ReturnDoctor_When_Get()
+        public void Should_ReturnDoctor_When_GetWithId()
         {
             var result = _doctorsController.Get(1);
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Id);
+            Assert.AreEqual("Martha", result.Name);
         }
 
-        private static List<Doctor> GetTestDoctors()
+        [Test]
+        public void Should_ReturnDoctors_When_Get()
         {
-            var doctors = new List<Doctor>();
-            doctors.Add(new Doctor{Id = 1, Name = "Name1", Surname = "Surname1", Specialization = Specialization.Trainee});
-            doctors.Add(new Doctor{Id = 2, Name = "Name2", Surname = "Surname2", Specialization = Specialization.Gynecology});
-            doctors.Add(new Doctor{Id = 3, Name = "Name3", Surname = "Surname3", Specialization = Specialization.Pediatrics});
-            doctors.Add(new Doctor{Id = 4, Name = "Name4", Surname = "Surname4", Specialization = Specialization.Radiology});
-            doctors.Add(new Doctor{Id = 5, Name = "Name5", Surname = "Surname5", Specialization = Specialization.Surgery});
-
-            return doctors;
+            var result = _doctorsController.Get();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count());
         }
     }
 }
