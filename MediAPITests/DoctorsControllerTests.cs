dcb82c8668941caa0ece5195b128cbb99be6ddd2
binas <matib95@gmail.com>
2021-01-29 20:38:39 +0100

    test: Add few tests for controllers

--- MediAPITests/DoctorsControllerTests.cs
+++ MediAPITests/DoctorsControllerTests.cs
@@ -1,9 +1,14 @@
+using System;
 using FakeItEasy;
+using Medi.Core.Domain;
 using Medi.WebApi.Controllers;
+using Medi.WebApi.DTO;
 using Medi.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace MediAPITests
 {
@@ -20,6 +25,57 @@ namespace MediAPITests
             _logger = A.Fake<ILogger<DoctorController>>();
             _doctorService = A.Fake<IDoctorService>();
             _doctorController = new DoctorController(_logger, _doctorService);
+        }
+
+        [Test]
+        public void Should_ReturnOkResult_When_Get()
+        {
+            var result = _doctorController.Get();
+            
+            A.CallTo(() => _doctorService.GetAll()).MustHaveHappenedOnceExactly();
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+        }
+
+        [Test]
+        public void Should_AddDoctorAndReturnCreated_When_Post()
+        {
+            var doctorDto = A.Fake<DoctorDto>();
+            doctorDto.Name = "MyName";
+            doctorDto.Surname = "Surname";
+            doctorDto.Specialization = Specialization.Surgery;
+
+            var result = _doctorController.Post(doctorDto);
+
+            A.CallTo(() => _doctorService.Add(doctorDto.Name, doctorDto.Surname, doctorDto.Specialization))
+                .MustHaveHappened();
+            Assert.AreEqual(typeof(CreatedResult), result.GetType());
+        }
+
+        [Test]
+        public void Should_ReturnOkResultWithDoctor_When_GetById()
+        {
+            var doctorDto = A.Fake<DoctorDto>();
+            var guid = Guid.NewGuid();
+            doctorDto.Id = guid.ToString();
+            doctorDto.Name = "MyName";
+            doctorDto.Surname = "Surname";
+            doctorDto.Specialization = Specialization.Surgery;
+            A.CallTo(() => _doctorService.Get(guid)).Returns(doctorDto);
+            
+            var result = _doctorController.Get(guid);
+            
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+        }
+
+        [Test]
+        public void Should_ReturnNotFound_When_NotFoundDoctorForId()
+        {
+            var guid = Guid.NewGuid();
+            A.CallTo(() => _doctorService.Get(guid)).Returns(null);
+
+            var result = _doctorController.Get(guid);
+
+            Assert.AreEqual(typeof(NotFoundResult), result.GetType());
         }
     }
 }
