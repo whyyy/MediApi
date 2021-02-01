e295da5a99355fb913d7e880757bbdd4ad11839d
binas <matib95@gmail.com>
2021-02-01 19:52:55 +0100

    test: Add test for GetAll method

---
+++ Medi.WebApiTests/Controllers/PatientsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http.Results;
+using Castle.Core.Logging;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace MediAPITests.Controllers
+{
+    [TestFixture]
+    public class PatientsControllerTests
+    {
+        private ILogger<PatientsController> _logger;
+        private IPatientService _patientService;
+        private PatientController _patientController;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _logger = A.Fake<ILogger<PatientsController>>();
+            _patientService = A.Fake<IPatientService>();
+            _patientController = new PatientController(_logger, _patientService);
+        }
+
+        [Test]
+        public void Should_ReturnPatientsAndOkResult_When_GetAll()
+        {
+            var result = _patientController.GetAll();
+
+            A.CallTo(() => _patientService.GetAll()).MustHaveHappened();
+            Assert.AreEqual(typeof(OkResult), result.GetType());
+        }
+    }
+}
