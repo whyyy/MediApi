197efd219e8fa21954714cfa48ea4ab66b3ff2cd
binas <matib95@gmail.com>
2021-04-04 16:50:40 +0200

    style: Update access modifier type

--- Medi.WebApi/Program.cs
+++ Medi.WebApi/Program.cs
@@ -19,7 +19,7 @@ namespace Medi.WebApi
             host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
