f4924525f7b9ff29caa701ea490663d31feee8e8
binas <matib95@gmail.com>
2021-04-05 15:50:14 +0200

    feature: Add migrations for new table

---
+++ Medi.Database/Migrations/20210405134313_Patient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Medi.Database.Migrations
+{
+    public partial class Patient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Occupation = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    LeadDoctorId = table.Column<Guid>(nullable: false),
+                    FirstDayInHospital = table.Column<DateTime>(nullable: false),
+                    LastDayInHospital = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
